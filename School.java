ec7271d6725528a30b29962e142c62bf0e978cd5
malickseye <seyemalick07@gmail.com>
2018-01-19 23:02:55 +0000

    modification de school

--- School.java
+++ School.java
@@ -2,4 +2,13 @@ package model;
 
 public class School {
 
+	private ArrayList<Talent> talents = new ArrayList<Talent>();
+	private static int effectif = 0;
+	
+	public void addTalent(Talent T){
+		if(T!=null){
+			talents.add(T);
+			effectif++;
+		}
+	}
 }
